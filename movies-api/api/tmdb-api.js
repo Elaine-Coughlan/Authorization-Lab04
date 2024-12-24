05b5f48ce438174fa35a77a17bb02366e4457d69
Elaine Coughlan <20067964@mail.wit.ie>
2024-12-24 19:43:35 +0000

    Authentictation lab - exercise (genres)

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 
-export const getUpcomingMovies = async () => {
+const getUpcomingMovies = async () => {
     try {
         const response = await fetch(
             `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
@@ -15,3 +15,22 @@ export const getUpcomingMovies = async () => {
         throw error;
     }
 };
+
+const getMovieGenres = async () => {
+    try {
+        const response = await fetch(
+            `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+            
+        );
+
+        if (!response.ok) {
+            throw new Error(response.json().message);
+        }
+
+        return await response.json();
+    } catch (error) {
+        throw error;
+    }
+};
+
+export { getUpcomingMovies, getMovieGenres};
